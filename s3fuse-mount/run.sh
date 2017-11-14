f56b017b28ef726f4b62f47554798c2a9fab2f79
Boris Ruppert <boris.ruppert@consol.de>
2017-11-14 12:07:40 +0100

    optional debug logging, formatting

--- s3fuse-mount/run.sh
+++ s3fuse-mount/run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+echo "######################################"
 echo "mounting ${S3_BUCKET} to ${MOUNTPOINT}"
+echo "######################################"
 if [ -z ${DEBUG} ]; then
   s3fs ${S3_BUCKET} ${MOUNTPOINT}
 else
+echo "######################################"
   echo "credentials used"
   echo "id: ${AWSACCESSKEYID}"
   echo "key: ${AWSSECRETACCESSKEY}"
+echo "######################################"
   s3fs ${S3_BUCKET} ${MOUNTPOINT} -d -d -f -o f2 -o curldbg
 fi
 ls -l ${MOUNTPOINT}
