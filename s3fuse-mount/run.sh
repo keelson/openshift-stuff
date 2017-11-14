fbfcc2a7192629bc05d1360c8ddca30cb5e434fa
Boris Ruppert <boris.ruppert@consol.de>
2017-11-14 11:04:09 +0100

    removed debug info

--- s3fuse-mount/run.sh
+++ s3fuse-mount/run.sh
@@ -3,6 +3,6 @@ PWFILE=/tmp/.s3
 echo ${S3_USER}:${S3_PASSWORD} > ${PWFILE}
 chmod 600 ${PWFILE}
 
-cat ${PWFILE}
+echo "mounting ${S3_BUCKET} to ${MOUNTPOINT}"
 s3fs ${S3_BUCKET} ${MOUNTPOINT} -o passwd_file=${PWFILE}
 ls -l ${MOUNTPOINT}
