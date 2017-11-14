d27f2b3f0c9a77d1184a1d2b5d7314360476719e
Boris Ruppert <boris.ruppert@consol.de>
2017-11-14 11:39:10 +0100

    S3 credentials will only be passed as environment variables

--- s3fuse-mount/run.sh
+++ s3fuse-mount/run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-PWFILE=/tmp/.s3
-echo ${S3_USER}:${S3_PASSWORD} > ${PWFILE}
-chmod 600 ${PWFILE}
-
 echo "mounting ${S3_BUCKET} to ${MOUNTPOINT}"
-s3fs ${S3_BUCKET} ${MOUNTPOINT} -o passwd_file=${PWFILE}
+s3fs ${S3_BUCKET} ${MOUNTPOINT}
 ls -l ${MOUNTPOINT}
